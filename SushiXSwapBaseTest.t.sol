9a725c752243f65bb69df2f1a8868f8e9450642c
vladmad115 <117020202+vladmad115@users.noreply.github.com>
2023-07-14 16:35:03 +0200

    Create SushiXSwapBaseTest.t.sol

---
+++ SushiXSwapBaseTest.t.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.0;
+
+import {SushiXSwapV2} from "../src/SushiXSwapV2.sol";
+import {StargateAdapter} from "../src/adapters/StargateAdapter.sol";
