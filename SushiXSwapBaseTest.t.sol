5f53489ecbc284d33cecb554d578803293c9c028
vladmad115 <117020202+vladmad115@users.noreply.github.com>
2023-07-17 15:15:03 +0200

    Update SushiXSwapBaseTest.t.sol

--- SushiXSwapBaseTest.t.sol
+++ SushiXSwapBaseTest.t.sol
@@ -5,3 +5,4 @@ import {SushiXSwapV2} from "../src/SushiXSwapV2.sol";
 import {StargateAdapter} from "../src/adapters/StargateAdapter.sol";
 import {IRouteProcessor} from "../src/interfaces/IRouteProcessor.sol";
 import {IWETH} from "../src/interfaces/IWETH.sol";
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
