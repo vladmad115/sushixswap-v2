65ca5bb29fa5318aa5304d915aeb5c8163df8f6b
vladmad115 <117020202+vladmad115@users.noreply.github.com>
2023-12-07 14:32:10 +0100

    Update SushiXSwapBaseTest.t.sol

--- SushiXSwapBaseTest.t.sol
+++ SushiXSwapBaseTest.t.sol
@@ -336,14 +336,4 @@ deal(address(weth), address(sushiXswap), amountToRescue);
                 route: computedRoute
             });
 
-        bytes memory rpd_encoded = abi.encode(rpd);
-
-        sushiXswap.swap(rpd_encoded);
-
-        vm.stopPrank();
-
-        assertEq(weth.balanceOf(user), 0, "weth balance should be 0");
-        assertGt(
-  usdc.balanceOf(user),
-            0,
-            "usdc balance should be greater than 0"
+      
